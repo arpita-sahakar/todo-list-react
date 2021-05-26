881527841036ad223a2e98b3596db476a96f6991
Arpita Saha <arpita.sahaa@gmail.com>
2021-05-26 19:18:20 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import Form from "./components/Form.jsx";
+import TodoList from "./components/TodoList.jsx"
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <h1>todolist</h1>
+      <h1>Arpita's Todo-List</h1>
+      <Form setInputText = {setInputText}/>
+      <TodoList/>
     </div>
   );
 }
